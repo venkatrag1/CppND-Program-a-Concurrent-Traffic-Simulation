3a941d90a26138d13b95dc879bf8ebd29cb3391e
Venkat Ramesh <venkatraghavan@fb.com>
2022-07-29 11:26:18 -0700

    Fix annotation

--- src/Intersection.cpp
+++ src/Intersection.cpp
@@ -87,7 +87,7 @@ void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle) {
   std::cout << "Intersection #" << _id << ": Vehicle #" << vehicle->getID()
             << " is granted entry." << std::endl;
 
-  // FP.6b : use the methods TrafficLight::getCurrentPhase and
+  // DONE: FP.6b : use the methods TrafficLight::getCurrentPhase and
   // TrafficLight::waitForGreen to block the execution until the traffic light
   // turns green.
   if (_trafficLight.getCurrentPhase() == TrafficLightPhase::red) {
